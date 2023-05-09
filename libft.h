fec435b0fb0e268faadbf671e7e53070092c83b8
jlozano- <jlozano-@student.42madrid.com>
2023-05-09 13:56:38 +0200

    Modify putchar prototype to return int

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jlozano- <jlozano-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 16:42:38 by jlozano-          #+#    #+#             */
-/*   Updated: 2022/09/14 12:36:59 by jlozano-         ###   ########.fr       */
+/*   Updated: 2023/05/09 13:54:55 by jlozano-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,7 @@ char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
-void	ft_putchar_fd(char c, int fd);
+int		ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
